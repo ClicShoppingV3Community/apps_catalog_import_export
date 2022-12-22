828cf92d2678dd6bc7225d36b9451a6fcb333014
ClicShopping <loic.richard2@gmail.com>
2022-12-22 13:59:46 -0500

    update

--- includes/ClicShopping/Apps/Catalog/ImportExport/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
+++ includes/ClicShopping/Apps/Catalog/ImportExport/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
@@ -4,7 +4,7 @@
    * @copyright 2008 - https://www.clicshopping.org
    * @Brand : ClicShopping(Tm) at Inpi all right Reserved
    * @Licence GPL 2 & MIT
-   * @licence MIT - Portion of osCommerce 2.4
+
    * @Info : https://www.clicshopping.org/forum/trademark/
    *
    */
@@ -46,7 +46,7 @@
       $cut_length = \strlen('CLICSHOPPING_APP_IMPORT_EXPORT_' . $this->code . '_');
 
       foreach ($this->getParameters() as $key) {
-        $p = strtolower(substr($key, $cut_length));
+        $p = mb_strtolower(substr($key, $cut_length));
 
         $class = 'ClicShopping\Apps\Catalog\ImportExport\Module\ClicShoppingAdmin\Config\\' . $this->code . '\Params\\' . $p;
 
@@ -100,7 +100,7 @@
       $cut_length = \strlen($cut);
 
       foreach ($this->getParameters() as $key) {
-        $p = strtolower(substr($key, $cut_length));
+        $p = mb_strtolower(substr($key, $cut_length));
 
         $class = 'ClicShopping\Apps\Catalog\ImportExport\Module\ClicShoppingAdmin\Config\\' . $this->code . '\Params\\' . $p;
 
